7ed3d14eb52dd6720d4665bbdc9394b599528ec5
filipe-cavalcanti-fpb <filipe.cazuza@academico.ifpb.edu.br>
2019-05-29 21:32:32 -0300

    atualizando o builder heap

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "Node.h"
 
+using namespace std;
+
 int main() {
     Node* nl = new Node();
-    nl->setFrequency(1234l);
+    nl->setFrequency(1234);
+
     Node* nr = new Node();
+    nr->setFrequency(12345);
+
     Node* root = new Node();
-    root->setRigth(nl);
-    root->setFrequency(123123);
-    std::cout << root->getRigth()->getFrequency() << std::endl;
+    root->setLeft(nr);
+    root->setRight(nl);
+    root->setLeft(nr);
+
+    cout << root->getLeft()->getFrequency() << endl;
+
     return 0;
 }

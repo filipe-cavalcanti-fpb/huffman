2ca84e58c98665e62bb0716597771e639297eace
Filipe Cavalcanti <filipe.cazuza@academico.ifpb.edu.br>
2019-06-02 21:08:15 -0300

    realizando testes

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -8,12 +8,22 @@ int main() {
     Heap *mimHeap = new Heap();
     mimHeap->setHeapLength(10);
     mimHeap->setHeapSize(10);
-    int a[10] = {4,1,3,2,16,9,10,14,8,7};
+    Node *n1 = new Node(4, 1l, NULL, NULL);
+    Node *n2 = new Node(1, 1l, NULL, NULL);
+    Node *n3 = new Node(3, 1l, NULL, NULL);
+    Node *n4 = new Node(2, 1l, NULL, NULL);
+    Node *n5 = new Node(16, 1l, NULL, NULL);
+    Node *n6 = new Node(9, 1l, NULL, NULL);
+    Node *n7 = new Node(10, 1l, NULL, NULL);
+    Node *n8 = new Node(14, 1l, NULL, NULL);
+    Node *n9 = new Node(8, 1l, NULL, NULL);
+    Node *n10 = new Node(7, 1l, NULL, NULL);
+    Node a[10] = {*n1,*n2,*n3,*n4,*n5,*n6,*n7,*n8,*n9,*n10};
     mimHeap->setHeap(a);
     mimHeap->buildMinHeap();
 
     for(int i = 0; i < mimHeap->getHeapSize(); i++) {
-        cout << mimHeap->getHeap()[i] << endl;
+        cout << mimHeap->getHeap()[i].getFrequency() << endl;
     }
 
     return 0;
--- huffman/src/tree/Node.h
+++ huffman/src/tree/Node.h
@@ -12,6 +12,8 @@ private:
     long frequency;
     long pixelValue;
 public:
+    Node(long frequency, long pixelValue, Node *right, Node *left);
+
     long getPixelValue() const;
 
     void setPixelValue(long pixelValue);
